8f4286f7e28969e26fd9f6df8f0abada95473f4e
Daniel Brazel <dbrazel4@gmail.com>
2018-01-31 21:26:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0
+
 function init() {
   document.body.addEventListener('keydown', function(e){
-    alert('Congratulations!')
+    const key = parseInt(e.detail || e.which);
+    
+    if (key === code[index]) {
+      index++;
+      
+      if (index === code.length) {
+        alert("Congratulations, Konami Code entered!");
+        
+        index = 0;
+      
+      } else {
+        index = 0;
+      }
+    }
   })
 
 }
